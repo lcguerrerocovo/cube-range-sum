ab607f39cefb21629bd275072fc5eeb4b349aded
Luis Guerrero <lguerrero@vivareal.com>
2017-02-18 02:29:45 -0500

    final changes to grid implementation and runnable to test hackerrank input

--- src/main/scala/cubesum/Grid.scala
+++ src/main/scala/cubesum/Grid.scala
@@ -4,16 +4,24 @@ package cubesum
   * Created by luisguerrero
   */
 
+object Grid {
+  var gridMap = collection.Map.empty[String,Grid]
+  def apply(dimensionLength: Int) = new Grid(dimensionLength)
+}
+
 class Grid(dimensionLength: Int) {
 
   type VecGrid = Vector[Vector[Vector[Long]]]
 
   private var grid: VecGrid = Vector.fill (dimensionLength, dimensionLength, dimensionLength) (0l)
 
+  // cache for meoization of recursive function calls
   private var cache = scala.collection.concurrent.TrieMap.empty[(Int,Int,Int), Long]
 
   sumOfVolume(dimensionLength-1,dimensionLength-1,dimensionLength-1)
-  
+
+  // 3d implementation of summed area table
+  // described here: https://en.wikipedia.org/wiki/Summed_area_table
   private lazy val sumOfVolume: ((Int,Int,Int)) => Long = {
     case (x,y,z) if (x < 0 || y < 0 || z < 0) => 0L
     case (x,y,z) => {
@@ -39,20 +47,15 @@ class Grid(dimensionLength: Int) {
   }
 
   private def queryVector(x: Int, y: Int, z: Int, x2: Int, y2: Int, z2: Int): Long = {
-
       (sumOfVolume((x2, y2, z2)) - sumOfVolume((x2, y-1, z2)) - sumOfVolume((x-1, y2, z2)) -
         sumOfVolume((x2, y2, z-1)) + sumOfVolume((x-1, y-1, z2)) + sumOfVolume((x2, y-1, z-1)) +
         sumOfVolume((x-1, y2, z-1)) - sumOfVolume((x-1, y-1, z-1)))
   }
 
+  // public methods to expose vector of vectors of vectors grid as non-zero indexed, starting at (1,1,1)
   def update(x: Int, y: Int, z: Int, value: Long)
     = updateVector(x-1,y-1,z-1,value)
 
   def query(x: Int, y: Int, z: Int, x2: Int, y2: Int, z2: Int)
     = queryVector(x-1,y-1,z-1,x2-1,y2-1,z2-1)
-}
-
-
-object Grid {
- def apply(dimensionLength: Int) = new Grid(dimensionLength)
 }
