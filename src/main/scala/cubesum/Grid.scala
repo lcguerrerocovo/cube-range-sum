3b85a2892c3715d427b022f2b9126ad4b4c9e7ba
Luis Guerrero <lguerrero@vivareal.com>
2017-02-17 23:52:29 -0500

    adding query logic and setting indices to start with (1,1,1)

--- src/main/scala/cubesum/Grid.scala
+++ src/main/scala/cubesum/Grid.scala
@@ -8,13 +8,13 @@ class Grid(dimensionLength: Int) {
 
   type VecGrid = Vector[Vector[Vector[Long]]]
 
-  var grid: VecGrid = Vector.fill (dimensionLength, dimensionLength, dimensionLength) (0l)
+  private var grid: VecGrid = Vector.fill (dimensionLength, dimensionLength, dimensionLength) (0l)
 
-  var cache = scala.collection.concurrent.TrieMap.empty[(Int,Int,Int), Long]
+  private var cache = scala.collection.concurrent.TrieMap.empty[(Int,Int,Int), Long]
 
   sumOfVolume(dimensionLength-1,dimensionLength-1,dimensionLength-1)
-
-  lazy val sumOfVolume: ((Int,Int,Int)) => Long = {
+  
+  private lazy val sumOfVolume: ((Int,Int,Int)) => Long = {
     case (x,y,z) if (x < 0 || y < 0 || z < 0) => 0L
     case (x,y,z) => {
       val (a,b,c,d,e,f,h) = (getVolume((x-1,y-1,z-1),sumOfVolume), getVolume((x,y,z-1),sumOfVolume),
@@ -24,19 +24,32 @@ class Grid(dimensionLength: Int) {
     }
   }
 
-  def getVolume(pos: (Int,Int,Int), f: ((Int,Int,Int)) => Long)
-  = cache getOrElseUpdate (pos, f(pos))
+  private def getVolume(pos: (Int,Int,Int), f: ((Int,Int,Int)) => Long)
+    = cache getOrElseUpdate (pos, f(pos))
   
-  def update(x: Int, y: Int, z: Int, value: Long): Unit = {
-    val prev = grid(x)(y)(z)
-    grid = grid.updated(x, grid(x).updated(y, grid(x)(y).updated(z, value)))
-    for {
-      i <- x until dimensionLength
-      j <- y until dimensionLength
-      k <- z until dimensionLength
-      sum = getVolume((i,j,k),sumOfVolume) - prev + value
-    } yield cache.put((i,j,k),sum)
+  private def updateVector(x: Int, y: Int, z: Int, value: Long): Unit = {
+      val prev = grid(x)(y)(z)
+      grid = grid.updated(x, grid(x).updated(y, grid(x)(y).updated(z, value)))
+      for {
+        i <- x until dimensionLength
+        j <- y until dimensionLength
+        k <- z until dimensionLength
+        sum = getVolume((i, j, k), sumOfVolume) - prev + value
+      } yield cache.put((i, j, k), sum)
   }
+
+  private def queryVector(x: Int, y: Int, z: Int, x2: Int, y2: Int, z2: Int): Long = {
+
+      (sumOfVolume((x2, y2, z2)) - sumOfVolume((x2, y-1, z2)) - sumOfVolume((x-1, y2, z2)) -
+        sumOfVolume((x2, y2, z-1)) + sumOfVolume((x-1, y-1, z2)) + sumOfVolume((x2, y-1, z-1)) +
+        sumOfVolume((x-1, y2, z-1)) - sumOfVolume((x-1, y-1, z-1)))
+  }
+
+  def update(x: Int, y: Int, z: Int, value: Long)
+    = updateVector(x-1,y-1,z-1,value)
+
+  def query(x: Int, y: Int, z: Int, x2: Int, y2: Int, z2: Int)
+    = queryVector(x-1,y-1,z-1,x2-1,y2-1,z2-1)
 }
 
 
--- src/test/scala/cubesum/GridSpec.scala
+++ src/test/scala/cubesum/GridSpec.scala
@@ -10,26 +10,26 @@ class GridSpec extends FlatSpec with Matchers {
 
  it should "change output of sum when grid is updated" in {
     val grid = Grid(10)
-    
-    grid.sumOfVolume(9,9,9) should equal (0L)
-    
-    grid.update(0,0,0,1)
 
-    grid.sumOfVolume(9,9,9) should equal (1L)
+    grid.query(1,1,1,10,10,10) should equal (0L)
+    
+    grid.update(1,1,1,1)
+
+    grid.query(1,1,1,10,10,10) should equal (1L)
 
     grid.update(1,2,2,1)
     grid.update(2,1,1,1)
     grid.update(1,3,1,1)
     grid.update(1,1,3,1)
 
-    grid.sumOfVolume(9,9,9) should equal (5L)
+    grid.query(1,1,1,10,10,10) should equal (5L)
   }
 
   it should "sum should stay the same in areas not affected by update" in {
     val grid = Grid(20)
 
-    grid.sumOfVolume(15,15,15) should equal (0L)
-    grid.sumOfVolume(5,5,5) should equal (0L)
+    grid.query(10,10,10,16,16,16) should equal (0L)
+    grid.query(1,1,1,6,6,6) should equal (0L)
 
     grid.update(12,12,12,1)
     grid.update(12,13,12,1)
@@ -37,7 +37,27 @@ class GridSpec extends FlatSpec with Matchers {
     grid.update(12,12,13,1)
     grid.update(13,13,13,1)
 
-    grid.sumOfVolume(15,15,15) should equal (5L)
-    grid.sumOfVolume(5,5,5) should equal (0L)
+    grid.query(10,10,10,16,16,16) should equal (5L)
+    grid.query(1,1,1,6,6,6) should equal (0L)
+  }
+
+  it should "respond with correct sums for queries (hackerrank1 test data)" in {
+    val grid = Grid(4)
+
+    grid.update(2,2,2,4)
+    grid.query(1,1,1,3,3,3) should equal (4L)
+
+    grid.update(1,1,1,23)
+    grid.query(2,2,2,4,4,4) should equal (4L)
+    grid.query(1,1,1,3,3,3) should equal (27L)
+  }
+
+  it should "respond with correct sums for queries (hackerrank2 test data)" in {
+    val grid = Grid(2)
+
+    grid.update(2,2,2,1)
+    grid.query(1,1,1,1,1,1) should equal (0L)
+    grid.query(1,1,1,2,2,2) should equal (1L)
+    grid.query(2,2,2,2,2,2) should equal (1L)
   }
 }
